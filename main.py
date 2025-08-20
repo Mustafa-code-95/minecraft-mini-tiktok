c8ff7a24fef406e08eb999beb28d4355563b335b
Mustafa <mustafa2014aksoy@gmail.com>
2025-08-20 19:55:29 +0200

    all files

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Sky(color=color.black)
 zahl = 7
 short = Animation('7', scale=(9, 5), loop=True, autoplay=True, z=-1)
 
-text = Text(text='`Minecraft mini Tiktok`', position=(-0.3, 0.4), color=color.white, scale=2)
+text = Text(text='`Minecraft mini Tiktok`', position=(-0.3, 0.4), color=color.gold, scale=2)
 
 
 def update():
@@ -23,7 +23,7 @@ def input(key):
     global short, zahl
     if key == 'escape' or key == 'q':
         application.quit()
-    if key == 'scroll up' and zahl != 13:
+    if key == 'scroll up' and zahl != 15:
         zahl += 1
         destroy(short)
         short = Animation(f'{zahl}', scale=(7, 4), loop=True, autoplay=True)
